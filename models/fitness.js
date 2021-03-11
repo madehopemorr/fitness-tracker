22f16f7f9cb0eae28126c510195cf62e78f80dd6
Madeline Morris <madehopemorr@gmail.com>
2021-03-10 16:10:26 -0800

    fitness models and index.js

---
+++ models/fitness.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const fitnessSchema = new Schema(
+    {
+        day: {
+            type: Date,
+            default: () => new Date(),
+        },
+        exercises: [
+            {
+                type: {
+                    type: String,
+                    trim: true,
+                    required: 'Please enter type of exercise',
+                },
+                name: {
+                    type: String,
+                    trim: true,
+                    required: 'Please enter the name of the exercise',
+                },
+                duration: {
+                    type: Number,
+                    required: 'What is the total duration in minutes of this exercise?',
+                },
+                weight: {
+                    type: Number,
+                },
+                distance: {
+                    type: Number,
+                },
+            },
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+    }
+);
+
+fitnessSchema.virtual('totalDuration').get(function () {
+    return this.exercises.reduce((total, exercise) => {
+        return total + exercise.duration;
+    }, 0);
+});
+
+const Fitness = mongoose.model('Fitness', fitnessSchema);
+
+module.exports = Fitness;
